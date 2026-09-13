004d2945a2f1cb468f43e252db61a37ab5a8aa3b
Colin Cai <therealcreative0708@gmail.com>
2026-09-12 21:50:48 -0400

    implement thermal os manager

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -1,7 +1,8 @@
 #include "thermal_mgr.h"
+#include "console.h"
 #include "errors.h"
 #include "lm75bd.h"
-#include "console.h"
+#include "logging.h"
 
 #include <FreeRTOS.h>
 #include <os_task.h>
@@ -42,19 +43,52 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 }
 
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
-  /* Send an event to the thermal manager queue */
+  if (xQueueSend(thermalMgrQueueHandle, event, 0) == errQUEUE_FULL)
+    return ERR_CODE_QUEUE_FULL;
 
   return ERR_CODE_SUCCESS;
 }
 
 void osHandlerLM75BD(void) {
-  /* Implement this function */
+  error_code_t errCode;
+
+  thermal_mgr_event_t event = {THERMAL_MGR_EVENT_OS_HANDLER_TRIG};
+  if (xQueueSend(thermalMgrQueueHandle, &event, 0) == errQUEUE_FULL)
+    LOG_ERROR_CODE(ERR_CODE_QUEUE_FULL);
 }
 
 static void thermalMgr(void *pvParameters) {
+  error_code_t errCode;
+
+  lm75bd_config_t *config = (lm75bd_config_t *)pvParameters;
+
+  thermal_mgr_event_t rxEvent;
+  float temperature;
+
   /* Implement this task */
   while (1) {
-    
+    xQueueReceive(thermalMgrQueueHandle, &rxEvent, portMAX_DELAY);
+    switch (rxEvent.type) {
+    case THERMAL_MGR_EVENT_MEASURE_TEMP_CMD:
+      LOG_IF_ERROR_CODE(readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temperature));
+      if (errCode != ERR_CODE_SUCCESS)
+        break;
+      addTemperatureTelemetry(temperature);
+      break;
+    case THERMAL_MGR_EVENT_OS_HANDLER_TRIG:
+      LOG_IF_ERROR_CODE(readTempLM75BD(LM75BD_OBC_I2C_ADDR, &temperature));
+      if (errCode != ERR_CODE_SUCCESS)
+        return;
+
+      if (temperature > config->hysteresisThresholdCelsius)
+        overTemperatureDetected();
+      else
+        safeOperatingConditions();
+      break;
+    default:
+      LOG_ERROR("Unhandled type in thermal_mgr_event_t: %d", rxEvent.type);
+      break;
+    }
   }
 }
 
